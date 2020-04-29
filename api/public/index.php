c4ef3347520ef50803177cb5b45487e8dd5fcd95
Julia <julsuniverse@gmail.com>
2020-04-29 17:13:55 +0300

    install Slim for API

--- api/public/index.php
+++ api/public/index.php
@@ -2,12 +2,42 @@
 
 declare(strict_types=1);
 
-chdir(dirname(__DIR__));
-require 'vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-header('Content-Type: application/json');
+require __DIR__ . '/../vendor/autoload.php';
 
-echo json_encode([
-    'name' => 'App API',
-    'version' => '1.0',
-]);
+$app = AppFactory::create();
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response, $args) {
+    $data = [
+        'name' => 'App API',
+        'version' => '1.0',
+    ];
+    $payload = json_encode($data);
+
+    $response->getBody()->write($payload);
+    return $response
+        ->withHeader('Content-Type', 'application/json');
+});
+
+// Run app
+$app->run();
