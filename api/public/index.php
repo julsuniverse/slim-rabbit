263a224080b57ac5b604b87943f404d426a1ef60
Julia <julsuniverse@gmail.com>
2020-04-29 19:10:22 +0300

    add api dotenv

--- api/public/index.php
+++ api/public/index.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 use Api\Http\Action;
 use Slim\Factory\AppFactory;
+use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists('.env')) {
+    (new Dotenv())->load('.env');
+}
 
 $app = AppFactory::create();
 
--- api/src/Http/Action/HomeAction.php
+++ api/src/Http/Action/HomeAction.php
@@ -7,10 +7,11 @@ namespace Api\Http\Action;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class HomeAction
+class HomeAction implements RequestHandlerInterface
 {
-    public function __invoke(ServerRequestInterface $request): ResponseInterface
+    public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $data = [
             'name' => 'App API',
